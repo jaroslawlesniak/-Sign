4fff224869c2297048eb00b31047de12b5edc04f
Jarosław Leśniak <jarek9244@gmail.com>
2021-11-06 19:32:43 +0100

    Resized document list container

--- src/controllers/Homepage.java
+++ src/controllers/Homepage.java
@@ -1,15 +1,8 @@
 package controllers;
 
 import enums.Scenes;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 import uSign.SceneManager;
 
 public class Homepage {
--- src/uSign/Main.java
+++ src/uSign/Main.java
@@ -2,11 +2,7 @@ package uSign;
 	
 import enums.Scenes;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 
 public class Main extends Application {
 	@Override
