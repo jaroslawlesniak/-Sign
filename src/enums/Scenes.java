0007ce501bcfdd0d4506a0ff9d97af39fd562ff1
Jarosław Leśniak <jarek9244@gmail.com>
2021-11-14 15:30:51 +0100

    Added documents list

--- src/enums/Scenes.java
+++ src/enums/Scenes.java
@@ -3,6 +3,7 @@ package enums;
 public enum Scenes {
 	HOMEPAGE("/fxml/Homepage.fxml"),
 	DOCUMENTS("/fxml/DocumentsList.fxml"),
+	DOCUMENT_PREVIEW("/fxml/DocumentPreview.fxml"),
 	NEW_SIGN("/fxml/NewSign.fxml");
 	
 	public String path;
